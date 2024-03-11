14f51a9f809a04a3e33fac1a6ea95d83930155e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-11 16:04:07 +0000

    vite 5.1.6

--- Formula/v/vite.rb
+++ Formula/v/vite.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Vite < Formula
   desc "Next generation frontend tooling. It's fast!"
   homepage "https://vitejs.dev/"
-  url "https://registry.npmjs.org/vite/-/vite-5.1.5.tgz"
-  sha256 "333846a6d7336cc75c96a46ec386d562232da15864ad37fc10acb954dc7db5ab"
+  url "https://registry.npmjs.org/vite/-/vite-5.1.6.tgz"
+  sha256 "6f0c4198269c9c71f92b80e3da28b3482d30a7d5252711a890a0ece5831d7dae"
   license "MIT"
 
   bottle do
